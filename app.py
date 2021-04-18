ab4140874bef3de987dc1f12bdad6c278300e96d
KALAM SARDAR <78305849+projectprogrammer057@users.noreply.github.com>
2021-04-18 23:36:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('clinic.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = [ "num_preg","glucose_conc","diastolic_bp","thickness","insulin","bmi","diab_pred","age","skin"]
+    
+    df= pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+        
+    if output == 1:
+        res_val = "** Diabatic Patient and Please Consult to the Doctor**"
+    else:
+        res_val = " Patient has no Diabaties and Enjoy your life"
+        
+
+    return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
+
+if __name__ == "__main__":
+    app.run()
